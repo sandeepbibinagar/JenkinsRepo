dd9f6c5b64f1282a560edd6b619be1554e8194f3
sandeepbibinagar <32436228+sandeepbibinagar@users.noreply.github.com>
2020-04-06 11:27:45 +0530

    Update TestWebService.java

--- WiniumDemo/src/test/java/testcases/TestWebService.java
+++ WiniumDemo/src/test/java/testcases/TestWebService.java
@@ -22,7 +22,7 @@ public class TestWebService {
 	public void testBody(){
 	res =RestAssured.get("https://reqres.in/api/users/2");
 	String data= res.asString();
-	System.out.println("eclipse changes is .... Data is : " +data);
+	System.out.println("Master changes is .... Data is : " +data);
 	
 	}
 }
